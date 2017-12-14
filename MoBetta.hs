4d34801ebb4907a48b5e1b1600fb1035414783d3
Michael <micseam@yahoo.com>
2017-12-14 13:02:41 -0800

    Created main module

---
+++ MoBetta.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import System.Environment
+import System.IO -- needed for file handling
+import Control.Monad.Trans.State.Lazy
+import Text.Megaparsec.Error
+
+
+import MoBettaParser
+-- import MoBettaEngine
+
+main = do
+  args <- getArgs
+  let fileName = head args
+  putstrLn $ "MoBetta running file: " ++ fileName ++ " ...\n"
+  handle <- openFile fileName ReadMode
+  contents <- hgetContents handle  -- get contents of file lazily
+  let pr = mbparse fileName contents
+  case pr of
+    Right prog -> evalStateT (makeProgram prog) emptyEnv
+    Left err -> putStrLn $ parseErrorPretty' contents err
+  hClose handle -- get out of my room, mom!
